b1fd1378fc0aeb6710df9dcfd484eee6c050149d
Roydano <roy.dano90@gmail.com>
2021-09-07 09:25:51 +0200

    configurato database per stampa su pagina prodotti comic

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,7 +3,16 @@
 
 @section('content')
 
-    <h1>prodotti</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores amet doloribus id, provident aliquid hic deserunt optio, quaerat nihil laudantium sint consequuntur iste quos, blanditiis ducimus perspiciatis voluptatem. Perferendis, repellat?</p>
+    <div class="comics">
+
+        @foreach($series as $serie)
+
+        <div class="card">
+           
+        </div>
+
+        @endforeach
+        
+    </div>
 
 @endsection
--- resources/views/partials/jumbo.blade.php
+++ resources/views/partials/jumbo.blade.php
@@ -1,3 +1,2 @@
 <div class="jumbo">
-    <img src="{{ asset('img/jumbotron.jpg') }}" alt="dc comics">
 </div>
